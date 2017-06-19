3048844cb531b37cfae46009601ba3ea0ace6524
hmoon3 <hmoon3@users.noreply.github.com>
2017-06-19 07:41:37 -0400

    Update inout.py

--- inout.py
+++ inout.py
@@ -12,8 +12,8 @@ while True:
     lb = input("How many ladybugs are there? ")
     try:
         lb = int(float(lb)) 
-        total += lb
     except ValueError:
-        print("BAD VALUE! EXITING NOW!!!" )
+        print("BAD VALUE ", lb, "! EXITING NOW!!!", sep = "" )
         sys.exit(1)
-    print("you have", total, "ladybugs so far")
+    total += lb
+    print("You have", total, "ladybugs so far.")
